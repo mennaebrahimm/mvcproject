0d95b69ee9e059d474308ee51f662d6a553c3374
Your Name <your.email@example.com>
2025-06-28 09:15:55 +0300

    updated homepage and footer section &product

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,26 +7,32 @@ namespace mvcproject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ProjectContext _context;   // DbContext
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ProjectContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _context.Products
+                                   .Where(p => !p.isDeleted)
+                                   .ToList();
+
+            return View(products);   
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
